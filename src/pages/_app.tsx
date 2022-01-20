48260b5938ec8af391e91aa053f2b12a2674884a
Murilo Vargas <muriraubach@gmail.com>
2022-01-20 16:37:23 -0300

    feat: add header component in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { AppProps } from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import Header from '../components/Header';
 import GlobalStyle from '../styles/global';
 import theme from '../styles/theme';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
