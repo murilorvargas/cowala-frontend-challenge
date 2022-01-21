5bdb5e7fb16e90daa59c6410902d8d713d25c2e8
Murilo Vargas <muriraubach@gmail.com>
2022-01-21 15:10:49 -0300

    feat: add local storage provider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import Header from '../components/Header';
+import { LocalStorageProvider } from '../hooks/useLocalStorage';
 import GlobalStyle from '../styles/global';
 import theme from '../styles/theme';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <LocalStorageProvider>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </LocalStorageProvider>
   );
 };
 
