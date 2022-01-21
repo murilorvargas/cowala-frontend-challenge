22de7cf440ae4d415b7c939a7a45d153c5fe4140
Murilo Vargas <muriraubach@gmail.com>
2022-01-20 23:58:37 -0300

    feat: add react hook form config

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,13 +1,59 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
+import api from '../../services/api';
 import Container from './styles';
 
+type userFormData = {
+  name: string;
+  profession: string;
+  number: string;
+  ip: string;
+};
+
+const userFormSchema = yup.object().shape({
+  name: yup.string().required(),
+  profession: yup.string().required(),
+  number: yup.string().required(),
+  ip: yup.string().required(),
+});
+
 const Form: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    setValue,
+    clearErrors,
+  } = useForm({
+    resolver: yupResolver(userFormSchema),
+  });
+
+  const handleFetchIp = async () => {
+    await api.get('api/ip').then(response => setValue('ip', response.data));
+    clearErrors('ip');
+  };
+
+  const onSubmit: SubmitHandler<userFormData> = values => {
+    console.log(values);
+    reset();
+  };
+
   return (
-    <Container>
+    <Container onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="name">Nome</label>
-        <input type="text" id="name" placeholder="Digite seu nome" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Digite seu nome"
+          className={errors.name && 'error'}
+          {...register('name')}
+        />
       </div>
       <div>
         <div>
@@ -15,24 +61,39 @@ const Form: React.FC = () => {
           <input
             type="text"
             id="profession"
+            name="profession"
             placeholder="Digite sua profissão"
+            className={errors.profession && 'error'}
+            {...register('profession')}
           />
         </div>
         <div>
-          <label htmlFor="phone-number">Celular</label>
+          <label htmlFor="number">Celular</label>
           <input
             type="text"
-            id="phone-number"
-            placeholder="Digite seu número de telefone"
+            id="number"
+            name="number"
+            placeholder="Digite seu número de celular"
+            className={errors.number && 'error'}
+            {...register('number')}
           />
         </div>
       </div>
       <div>
         <div>
           <label htmlFor="ip">Meu IP</label>
-          <input type="text" id="ip" disabled />
+          <input
+            type="text"
+            name="ip"
+            id="ip"
+            {...register('ip')}
+            readOnly
+            className={errors.ip && 'error'}
+          />
         </div>
-        <button type="button">ENCONTRAR IP</button>
+        <button type="button" onClick={handleFetchIp}>
+          ENCONTRAR IP
+        </button>
       </div>
       <div>
         <button type="submit">SALVAR</button>
