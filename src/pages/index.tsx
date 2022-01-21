2f66e66744a530250659e1ddd963a9e3583f389a
Murilo Vargas <muriraubach@gmail.com>
2022-01-21 15:12:01 -0300

    feat: pass properties to form on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,26 @@ import Head from 'next/head';
 import React from 'react';
 
 import Form from '../components/Form';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 import Container from '../styles/pages/Home';
 
-const pages: React.FC = () => {
+const Home: React.FC = () => {
+  const { clearLocalStorage, createLocalStorage, userData } = useLocalStorage();
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <Container>
-        <Form />
+        <Form
+          clearLocalStorage={clearLocalStorage}
+          createLocalStorage={createLocalStorage}
+          userData={userData}
+        />
       </Container>
     </>
   );
 };
 
-export default pages;
+export default Home;
